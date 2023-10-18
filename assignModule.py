2245de61959331185856aa6e637685d5b605d919
MachineCreation <142163578+MachineCreation@users.noreply.github.com>
2023-10-18 13:29:52 -0700

    Add files via upload

---
+++ assignModule.py
@@ -0,0 +1,19 @@
+def areaCal(length,width):
+    '''
+    funtion for calculating area of a space. Expects two
+    arguments length and width
+    '''
+    return length * width
+
+def circCal(rad):
+    '''
+    funtion for calculating the circumference of a 
+    circle given its radius. Expects one argument "rad"
+    for the radius.
+
+    If diameter(dia) is used try syntax:
+
+    circCal(dia/2)
+    '''
+    pi = 22/7
+    return 2 * rad * pi
